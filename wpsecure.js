495458c3f57243a74ab02d416561f9c1d353eaa5
Dhiraj Gupta <dhirajcoder777@gmail.com>
2026-02-20 12:21:35 +0530

    Comment out style removal code in wpsecure.js

--- wpsecure.js
+++ wpsecure.js
@@ -1,23 +1,24 @@
-window.addEventListener("load", function () {
+// window.addEventListener("load", function () {
 
-    // Remove all external stylesheets
-    document.querySelectorAll('link[rel="stylesheet"]').forEach(function(el){
-        el.remove();
-    });
+//     // Remove all external stylesheets
+//     document.querySelectorAll('link[rel="stylesheet"]').forEach(function(el){
+//         el.remove();
+//     });
 
-    // Remove all <style> tags
-    document.querySelectorAll('style').forEach(function(el){
-        el.remove();
-    });
+//     // Remove all <style> tags
+//     document.querySelectorAll('style').forEach(function(el){
+//         el.remove();
+//     });
 
-    // Remove all inline styles
-    document.querySelectorAll('*').forEach(function(el){
-        el.removeAttribute('style');
-    });
+//     // Remove all inline styles
+//     document.querySelectorAll('*').forEach(function(el){
+//         el.removeAttribute('style');
+//     });
 
-    });
+//     });
 
 console.log("All Clear");
+
 
 
 
