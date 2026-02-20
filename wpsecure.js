d69ffc126d2420512925902fa98b6560e4be3c3f
Dhiraj Gupta <dhirajcoder777@gmail.com>
2026-02-20 11:53:58 +0530

    Wordpress se

---
+++ wpsecure.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function () {
+
+    // Remove all external stylesheets
+    document.querySelectorAll('link[rel="stylesheet"]').forEach(function(el){
+        el.remove();
+    });
+
+    // Remove all <style> tags
+    document.querySelectorAll('style').forEach(function(el){
+        el.remove();
+    });
+
+    // Remove all inline styles
+    document.querySelectorAll('*').forEach(function(el){
+        el.removeAttribute('style');
+    });
+
+    console.log("All Clear");
+
+});
